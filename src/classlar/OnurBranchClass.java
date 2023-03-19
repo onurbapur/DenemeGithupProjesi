a0ff9e9c811fa4b504a74cdb14d28c6e98a955ff
onurbapur <onurrbapurr@gmail.com>
2023-03-19 15:24:20 +0300

    commit iki

---
+++ src/classlar/OnurBranchClass.java
@@ -0,0 +1,7 @@
+package classlar;
+
+public class OnurBranchClass {
+    public static void main(String[] args) {
+        System.out.println("Selamlar dostumm");
+    }
+}
---
+++ src/classlar/asddff.java
@@ -0,0 +1,4 @@
+package classlar;
+
+public class asddff {
+}
